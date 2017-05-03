82173ff7d28b1349b185726cf66a0225481bf4c5
mikuzune <maxwfarley@live.co.uk>
2017-05-03 02:43:46 +0100

    Adding AI

---
+++ Director Magnetic/Assets/Scripts/AI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+
+	//Transform playerTransform;
+	Transform wallTransform;
+	// Use this for initialization
+	void Start () {
+
+		//playerTransform = GameObject.FindGameObjectWithTag ("Player").transform;
+		wallTransform = GameObject.FindGameObjectWithTag ("TrainWall").transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (playerTransform.position - transform.position), 3.0f * Time.deltaTime);
+		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (wallTransform.position - transform.position), 3.0f * Time.deltaTime);
+		transform.position += transform.forward * 3.0f * Time.deltaTime;
+	}
+}
