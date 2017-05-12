00d32ede97a9db867e66613b81059e9ad62d9094
mikuzune <maxwfarley@live.co.uk>
2017-05-12 17:27:18 +0100

    smooth rotations to mouse look

---
+++ Assets/Scripts/RotaterScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotaterScript : MonoBehaviour {
+
+	Vector3 mousePos;
+	public Transform target;
+	Vector3 objectPos;
+	float angle;
+	
+	// Update is called once per frame
+	void Update () {
+		mousePos = Input.mousePosition;
+		mousePos.z = 5.23f;
+		objectPos = Camera.main.WorldToScreenPoint (target.position);
+		mousePos.x = mousePos.x - objectPos.x;
+		mousePos.y = mousePos.y - objectPos.y;
+		angle = Mathf.Atan2 (mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle));
+	}
+}
--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -1,61 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class playerController : MonoBehaviour {
-	bool seesMagnet = false;
-	Vector3 seenMagentPos = new Vector3(0,0,0);
-	public float startMagnetSpeed;
-	public GameObject lastMagnet;
-	public float magnetCharge;
-	public float magnetChargeIncrease;
-	bool goingRight;
-	// Use this for initialization
+public class characterController : MonoBehaviour {
 
+	public float MoveSpeed;
+	float magnetCharge;
+	public float magnetChargeIncrease;
+	public GameObject lastMagnet;
+	bool seesMagnet = false;
+	bool goingRight = false;
+	Vector3 seenMagentPos = new Vector3(0,0,0);
 	Rigidbody magnetRB;
 
-	public float moveSpeed;
-
-	public int playerScore;
-	void Start () {	}
-
-	public void LoadScene(int level)
-	{
-		LoadScene (level);
-	}
-		
-
-	void playerMove(){
-
-		float move;
-		if (Input.GetKey(KeyCode.D)) {
-			move = moveSpeed;
-			goingRight = true;
-		}else if (Input.GetKey(KeyCode.A)) {
-			move = -moveSpeed;
-			goingRight = false;
-		} else {
-			move = 0;
-		}
-		gameObject.GetComponent<Rigidbody> ().velocity = new Vector3(move,gameObject.GetComponent<Rigidbody>().velocity.y,0);
-
-	}
 
 	void magnetism(){
 		if (Input.GetMouseButton(0) && seesMagnet == true) {
 			float distanceFromPlayer;
 			distanceFromPlayer = transform.position.x - lastMagnet.transform.position.x;
-
+			
 			if (distanceFromPlayer < 0) {
 				distanceFromPlayer = -distanceFromPlayer;
 			}
-
+			
 			if (distanceFromPlayer > 1.0f) {
 				transform.position = Vector3.MoveTowards (transform.position, seenMagentPos, 0.5f);
-			} else {
-
-			}
+			} 
 		}
-
+		
 		if (Input.GetKey("e") && seesMagnet == true) {
 			
 			float distanceFromPlayer;
@@ -64,28 +35,28 @@ public class playerController : MonoBehaviour {
 			if (distanceFromPlayer < 0) {
 				distanceFromPlayer = -distanceFromPlayer;
 			}
-
+			
 			if (distanceFromPlayer > 3.5f) {
 				lastMagnet.transform.position = Vector3.MoveTowards (seenMagentPos, transform.position, 0.5f);
 			} else {
 				lastMagnet.transform.position = seenMagentPos;
 				lastMagnet.GetComponent<AI> ().heldStill = true;
 			}
-
+			
 			if (lastMagnet.GetComponent<AI> ().heldStill == true) {
 				Vector3 tempPos = GameObject.FindGameObjectWithTag ("MagnetHolder").transform.position;
 				lastMagnet.transform.position = tempPos;
 			}
-
+			
 			magnetCharge += magnetChargeIncrease;
-            
+			
 		}
-
+		
 		if (Input.GetKeyUp("e") && seesMagnet == true) {
 			float tempVal = 0;
 			if (magnetCharge > 20.0f) {
-                lastMagnet.GetComponent<CapsuleCollider>().enabled = false;
-                if (goingRight) {
+				lastMagnet.GetComponent<CapsuleCollider>().enabled = false;
+				if (goingRight) {
 					tempVal = 50.0f;
 				} else {
 					tempVal = -50.0f;
@@ -98,14 +69,39 @@ public class playerController : MonoBehaviour {
 			lastMagnet.GetComponent<AI> ().beingPulled = false;
 		}
 	}
-
-	void Update () {
-		if(Input.GetKeyDown("p")){
-			LoadScene(0);
+	
+	void playerMove(){
+		float tempMove;
+		if (Input.GetKey (KeyCode.D)) {
+			tempMove = MoveSpeed;
+			goingRight = true;
+		} else if (Input.GetKey (KeyCode.A)) {
+			tempMove = -MoveSpeed;
+			goingRight = false;
+		} else {
+			tempMove = 0;
 		}
+
+		gameObject.GetComponent<Rigidbody> ().velocity = new Vector3 (tempMove,gameObject.GetComponent<Rigidbody>().velocity.y,0);
+	}
+
+	// Update is called once per frame
+	void Update () {
 		playerMove ();
 		magnetism ();
 	}
-	
 
+	void OnTriggerStay(Collider coll){
+		if (coll.tag == "Magnetic") {
+			seesMagnet = true;
+			lastMagnet = coll.gameObject;
+			seenMagentPos = coll.transform.position;
+			magnetRB = coll.gameObject.GetComponent<Rigidbody> ();
+		}
+	}
+	void OnTriggerExit(Collider coll){
+		seesMagnet = false;
+		lastMagnet.GetComponent<AI> ().beingPulled = false;
+		lastMagnet.GetComponent<AI> ().heldStill = false;
+	}
 }
